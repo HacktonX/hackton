f1783b8f0554887b20dc2a5e0debc2f5f111065f
orielyem <orielyemini@gmail.com>
2018-03-12 01:34:40 +0200

    client side

--- coffee/public/javascripts/index.js
+++ coffee/public/javascripts/index.js
@@ -1,1 +1,9 @@
-var MyApp = angular.module('MainController',[]);
+var MyApp = angular.module('MyApp',[]);
+
+angular.module('MyApp', []).controller('MainController', function($scope) {
+    $scope.names = [
+        {name:'קפה קפה',ProdName1:'קפה',ProdName2:'מאפה',AmountProd1:13,AmountProd2:4,pic:'Cafe.png'},
+        {name:'קפה גו',ProdName1:'קפה',ProdName2:'מאפה',AmountProd1:1,AmountProd2:2,pic:'JoeLogo.jpg'},
+        {name:'בורגראנץ',ProdName1:'ציפס',ProdName2:'קולה',AmountProd1:3,AmountProd2:9,pic:'Burger.png'}
+    ];
+});
