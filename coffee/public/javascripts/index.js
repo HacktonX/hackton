8340f757221cee5fe8dfcf27f31791b94cd2a3cc
Unknown <dudizohar1997@gmail.com>
2018-03-12 01:36:50 +0200

    Auto stash before merge of "master" and "origin/master"
    david changes

--- coffee/public/javascripts/index.js
+++ coffee/public/javascripts/index.js
@@ -1,1 +1,20 @@
-var MyApp = angular.module('MainController',[]);
+var app = angular.module("myApp", ["ngRoute"]);
+
+
+
+
+
+app.config(function($routeProvider) {
+    $routeProvider
+    .when("/", {
+        templateUrl : "WelcomeJumbotron.html"
+    })
+    .when("/ChooseRest", {
+        templateUrl : "Second.html"
+    })
+    .when("/ChoosePortion", {
+        templateUrl : "Third.html"
+    }) .when("/Pay", {
+        templateUrl : "Fourth.html"
+    });
+});
