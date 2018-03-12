fc418374abcab7c9cb2d23f9fd05518acb266742
DavidZohar1 <dudizohar1997@gmail.com>
2018-03-12 03:38:46 +0200

    client side

--- coffee/public/javascripts/index.js
+++ coffee/public/javascripts/index.js
@@ -5,21 +5,28 @@ var app = angular.module("MyApp", ["ngRoute"]);
 app.config(function($routeProvider) {
     $routeProvider
     .when("/", {
-        templateUrl : "WelcomeJumbotron.html"
+        templateUrl : "/Start"
     })
     .when("/ChooseRest", {
-        templateUrl : "Second.html"
+        templateUrl : "/RestChoose"
     })
     .when("/ChoosePortion", {
-        templateUrl : "Third.html"
+        templateUrl : "/PortionChoose"
     }) .when("/Pay", {
         templateUrl : "Fourth.html"
     });
+    });
 
-angular.module('MyApp', []).controller('MainController', function($scope) {
+app.controller('MainController', function($scope) {
+    $scope.currShop=[];
     $scope.names = [
-        {name:'קפה קפה',ProdName1:'קפה',ProdName2:'מאפה',AmountProd1:13,AmountProd2:4,pic:'Cafe.png'},
-        {name:'קפה גו',ProdName1:'קפה',ProdName2:'מאפה',AmountProd1:1,AmountProd2:2,pic:'JoeLogo.jpg'},
-        {name:'בורגראנץ',ProdName1:'ציפס',ProdName2:'קולה',AmountProd1:3,AmountProd2:9,pic:'Burger.png'}
+        {name:'קפה קפה',ProdName1:'קפה',ProdName2:'מאפה',AmountProd1:13,AmountProd2:4,pic:'/img/Cafe.png'},
+        {name:'קפה גו',ProdName1:'קפה',ProdName2:'מאפה',AmountProd1:1,AmountProd2:2,pic:'/img/JoeLogo.jpg'},
+        {name:'בורגראנץ',ProdName1:'ציפס',ProdName2:'קולה',AmountProd1:3,AmountProd2:9,pic:'/img/Burger.png'}
     ];
+    
+    $scope.TakeShop = function(arr) {
+       $scope.currShop = arr; 
+    };
+    
 });
